28bfc90c0eb9196994b281975b62fcd89624ae5f
DPU J S <DPU J S@DESKTOP-12TU7QI>
2020-10-05 10:17:55 +0530

    Add project files.

---
+++ EventAndEventArgs/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace EventAndEventArgs
+{
+    
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var worker = new Worker();
+            worker.WorkPerformed += new EventHandler<WorkPerformedEventArgs>(Worker_WorkPerformed);
+            worker.WorkCompleted += new EventHandler(Worker_WorkCompleted);
+            worker.DoWork(8,WorkType.GenerateReports);
+            
+            Console.Read();
+        }
+
+        static void Worker_WorkPerformed(object sender, WorkPerformedEventArgs e)
+        {
+            Console.WriteLine("Hours worked : " + e.Hours + " " + e.WorkType);
+        }
+        static void Worker_WorkCompleted(object sender, EventArgs e)
+        {
+            Console.WriteLine("Work is done");
+
+        }
+       
+    }
+    public enum WorkType
+    {
+        GoToMeetings,
+        Golf,
+        GenerateReports
+    }
+}
---
+++ EventAndEventArgs/WorkPerformedEventArgs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventAndEventArgs
+{
+    public class WorkPerformedEventArgs : EventArgs
+    {
+        public WorkPerformedEventArgs(int hours, WorkType workType)
+        {
+            Hours = hours;
+            WorkType = workType;
+        }
+
+        public int Hours { get; set; }
+        public WorkType WorkType { get; set; }
+
+    }
+}
